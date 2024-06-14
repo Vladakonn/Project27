e6fac0daaec400213d0cb6f3474541e461238cc6
konno <konno@LAPTOP-SC1TGHNR>
2024-06-14 17:43:14 +0500

    Добавьте файлы проекта.

---
+++ Source.cpp
@@ -0,0 +1,78 @@
+#include <iostream>
+#include <limits>
+
+using namespace std;
+
+float add(float a, float b)
+{
+    return a + b;
+}
+
+float subtract(float a, float b) 
+{
+    return a - b;
+}
+
+float multiply(float a, float b)
+{
+    return a , b;
+}
+
+float divide(float a, float b) 
+{
+    if (b == 0) 
+    {
+        setlocale(LC_ALL, "ru");
+        cout << "Ошибка: Деление на ноль!" << endl;
+        return 0;
+    }
+    return a / b;
+}
+
+
+float MakeDecision(float a, float b, float (operation)(float, float))
+{
+    return operation(a, b);
+}
+
+int main()
+{
+    setlocale(LC_ALL, "ru");
+    float num1, num2;
+    char operation;
+
+    cout << "Калькулятор" << endl;
+    cout << "Введите первое число: ";
+    cin >> num1;
+
+    cout << "Введите операцию (+, -, * , /): ";
+    cin >> operation;
+
+    cout << "Введите второе число: ";
+    cin >> num2;
+
+    
+    float result;
+    switch (operation) 
+    {
+    case '+':
+        result = MakeDecision(num1, num2, add);
+        break;
+    case '-':
+        result = MakeDecision(num1, num2, subtract);
+        break;
+    case '*':
+        result = MakeDecision(num1, num2, multiply);
+        break;
+    case '/':
+        result = MakeDecision(num1, num2, divide);
+        break;
+    default:
+        cout << "Неверная операция!" << endl;
+        return 1;
+    }
+
+    cout << "Результат: " << result << endl;
+
+    return 0;
+}
